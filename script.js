44c2bc6ba8b7333d75b22a577acaf97b156d57d5
Michael Bashford <mikebashford@gmail.com>
2022-05-24 18:38:11 -0500

    Added api calls for top 30 crypto and price grabber

---
+++ script.js
@@ -0,0 +1,12 @@
+const COIN_INFO_ENDPOINT =
+  "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=30&tsym=USD";
+const COIN_PRICE_ENDPOINT =
+  "https://min-api.cryptocompare.com/data/pricemulti?tsyms=USD&fsyms=";
+
+class CryptoApp {
+  constructor() {
+    this.coinData = {};
+  }
+
+  start() {}
+}
